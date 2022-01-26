32f354b16a65794cce117831dfb0de25d4899c43
SvetlyiD <SvetlyiD@yandex.ru>
2022-01-26 15:01:19 +0400

    'settings'

--- ios/yourappnameTests/yourappnameTests.m
+++ ios/yourappnameTests/yourappnameTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface qdigitalcoreTests : XCTestCase
+@interface yourappnameTests : XCTestCase
 
 @end
 
-@implementation qdigitalcoreTests
+@implementation yourappnameTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
